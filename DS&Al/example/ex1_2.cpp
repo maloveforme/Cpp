22f563a33263d7cc62c9b6e73afe98c1dcb4a9c3
maloveforme <studyseok8466@gmail.com>
2023-01-28 23:45:45 +0900

    Update CH1 example solved

---
+++ DS&Al/example/ex1_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <array>
+#include <iostream>
+#include <type_traits>
+
+template<typename ... Args> // 가변 인자를 갖는 템플릿
+auto build_array(Args&&... args)->std::array<typename std::common_type<Args...>::type, 
+	sizeof...(args)> // sizeof... (args)라는 크기를 갖고, array<common_type<Args...>::type의 배열
+{
+	using commonType = typename std::common_type<Args...>::type;
+	return { std::forward<commonType>((Args&&)args)... };
+} // -> 의 의미는 후행 반환 형식 
+
+int main()
+{
+	auto data = build_array(1, 0u, 'a', 3.2f, false);
+
+	for (auto i : data)
+		std::cout << i << " ";
+	std::cout << std::endl;
+}
